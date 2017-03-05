4b15d7253da1c13408ec8e60da1b7d1d4b4b7ebd
P Reddy <prajwalreddy@Ps-iMac.local>
2017-03-05 19:20:27 +0530

    Initial Commit

---
+++ viewcontroller-example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  viewcontroller-example
+//
+//  Created by Prajwal Reddy on 05/03/17.
+//  Copyright © 2017 Prajwal Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
